bef2f0f6bdf8ff38a8dcee8e82f13a3c6368308b
snah0902 <103289974+snah0902@users.noreply.github.com>
2022-04-10 15:38:47 -0400

    jetpack joyride 1

--- jetpack-joyride.py
+++ jetpack-joyride.py
@@ -1,6 +1,7 @@
 from cmu_cs3_graphics import *
 import time
 import random
+import math
 
 class Zapper(object):
     
@@ -19,10 +20,11 @@ def onAppStart(app):
     app.circleVelocity = 0
     app.isSpaceHeld = False
     app.jetpackAcceleration = 0
-    app.stepsPerSecond = 60
+    app.stepsPerSecond = 45
     app.ticks = 0
     app.speed = 5
     app.obstacleList = [ ]
+    app.zapperR = 15
     
     # testing randomly picking an obstacle
     app.events = ['Missile', 'Zapper', 'Laser', 'Coins']
@@ -45,21 +47,6 @@ def jetpack(app, key=None):
         app.jetpackAcceleration = 0.5
     else:
         app.jetpackAcceleration = 0
-
-
-def redrawAll(app):
-    drawCircle(app.playerX, app.playerY, app.playerR)
-    drawLabel(app.ticks // 10, app.width / 2, app.height / 2)
-    
-    # draws obstacles in obstacleList
-    for obstacle in app.obstacleList:
-        drawCircle(obstacle.firstZapCoords[0], obstacle.firstZapCoords[1],
-                   obstacle.radius, fill='yellow')
-        drawCircle(obstacle.secondZapCoords[0], obstacle.secondZapCoords[1],
-                   obstacle.radius, fill='yellow')
-        drawLine(obstacle.firstZapCoords[0], obstacle.firstZapCoords[1],
-                 obstacle.secondZapCoords[0], obstacle.secondZapCoords[1])
-    
     
 # returns the net acceleration of player
 def netAcceleration(app):
@@ -88,36 +75,86 @@ def dplayerY(app, acceleration):
 
 # creates zapper obstacle
 def createZapper(app):
+    bounds = 100   
     firstZapX = app.width
-    firstZapY = random.randint(20, app.height - 50)
-    secondZapX = firstZapX + random.choice([-15, 0, 15])
-    secondZapY = firstZapY + random.randint(50, 200)
-    while secondZapY < 0 or secondZapY > app.height:
-        print(secondZapY)
-        secondZapY = firstZapY + random.randint(50, 200)
+    firstZapY = random.randint(bounds, app.height - bounds)
+    secondZapDistance = random.randint(app.zapperR * 2, bounds)
+    angle = random.choice([0, math.pi/4, math.pi/2, 3*math.pi/4, math.pi,
+                        5*math.pi/4, 3*math.pi/2, 7*math.pi/4, 2*math.pi])
+    secondZapX = firstZapX + (secondZapDistance * math.cos(angle))
+    secondZapY = firstZapY + (secondZapDistance * math.sin(angle))
     firstZapCoords = (firstZapX, firstZapY)
     secondZapCoords = (secondZapX, secondZapY)
     newZapper = Zapper(firstZapCoords, secondZapCoords)
     app.obstacleList.append(newZapper)
 
+# returns distance between two points
+def distance(x0, y0, x1, y1):
+    return ((y1 - y0)**2 + (x1 - x0)**2)**0.5
 
-def onStep(app):
-    app.ticks += 1
-    if app.ticks % 40 == 0:
-        createZapper(app)
+
+
+
+# checks if player collides with any zappers
+def checkZapperCollisions(app):
+    for obstacle in app.obstacleList:
+        
+        firstZapX = obstacle.firstZapCoords[0]
+        firstZapY = obstacle.firstZapCoords[1]
+
+        # checking collisions of zap circles
+        if (distance(app.playerX, app.playerY, firstZapX, firstZapY) <=
+           (app.playerR + app.zapperR)):
+            print('death skull emoji')
+        secondZapX = obstacle.secondZapCoords[0]
+        secondZapY = obstacle.secondZapCoords[1]
+        if (distance(app.playerX, app.playerY, secondZapX, secondZapY) <=
+           (app.playerR + app.zapperR)):
+           print('death skull emoji')
+
+        # checking collisions of zap line (currently broken)
+        if obstacle.secondZapCoords[0] - obstacle.firstZapCoords[0] == 0:
+            slope = 0
+        else:
+            slope = -((obstacle.secondZapCoords[1] - obstacle.firstZapCoords[1])
+                    /(obstacle.secondZapCoords[0] - obstacle.firstZapCoords[0]))
+        a = slope
+        b = -1
+        c = -(slope * obstacle.firstZapCoords[0]) + obstacle.firstZapCoords[1]
+        dist = ((abs(a * app.playerX + b * app.playerY + c)) /
+                math.sqrt(a ** 2 + b ** 2))
     
+        if (app.playerR >= dist):
+            print(dist)
+        
+
+
+
+# moves and deletes all the zappers every step
+def moveAndDeleteZappers(app):
     i = 0        
     while i < len(app.obstacleList):
         obstacle = app.obstacleList[i]
-        print(obstacle.firstZapCoords)
-        obstacle.firstZapCoords[0] -= app.speed
-        obstacle.secondZapCoords[0] -= app.speed
-        
+        firstZapX = obstacle.firstZapCoords[0]
+        secondZapX = obstacle.secondZapCoords[0]
+        firstZapX -= app.speed
+        secondZapX -= app.speed
+        obstacle.firstZapCoords = (firstZapX, obstacle.firstZapCoords[1])
+        obstacle.secondZapCoords = (secondZapX, obstacle.secondZapCoords[1])
         
         if obstacle.secondZapCoords[0] < 0:
             app.obstacleList.pop(i)
         else:
             i += 1
+
+def onStep(app):
+    app.ticks += 1
+
+
+    if app.ticks % 40 == 0:
+        createZapper(app)
+    moveAndDeleteZappers(app)
+    checkZapperCollisions(app)
 
     
     # adjusts player acceleration, velocity, and position
@@ -128,6 +165,20 @@ def onStep(app):
     acceleration = netAcceleration(app)
     dplayerY(app, acceleration)
     
+def redrawAll(app):
+    drawCircle(app.playerX, app.playerY, app.playerR)
+    drawLabel(app.ticks // 10, app.width / 2, app.height / 2)
+    
+    # draws obstacles in obstacleList
+    for obstacle in app.obstacleList:
+        drawCircle(obstacle.firstZapCoords[0], obstacle.firstZapCoords[1],
+                   obstacle.radius, fill='yellow')
+        drawCircle(obstacle.secondZapCoords[0], obstacle.secondZapCoords[1],
+                   obstacle.radius, fill='yellow')
+        drawLine(obstacle.firstZapCoords[0], obstacle.firstZapCoords[1],
+                 obstacle.secondZapCoords[0], obstacle.secondZapCoords[1])
+    
+
 
 
 def main():
